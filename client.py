c594725ced865118eee3e20084b8870d608a407c
David Hany <140831127+davidhany306@users.noreply.github.com>
2026-05-04 23:59:58 +0300

    Refactor client.py for improved command handling

--- client.py
+++ client.py
@@ -1,20 +1,20 @@
-import socket
-import sys
+import socket   ## library to connect to server 
 
-def send_command(host, port, command):
+
+def send_command(host, port, command):  ##(ip address, port number, command to send) 
     try:
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            s.connect((host, port))
-            s.sendall((command + "\n").encode())
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s: ## create a socket object  (af_inet for ipv4, sock_stream for tcp)
+            s.connect((host, port)) ## connect to the server using the provided host and port
+            s.sendall((command + "\n").encode()) ## send the command to the server fe 25rha /n, encoding it to bytes and adding a newline character
 
-            reply = b""
+            reply = b""  ## variable of bytes 
             while b"\n" not in reply:
                 chunk = s.recv(1024)
                 if not chunk:
                     break
                 reply += chunk
 
-            reply = reply.decode().strip()
+            reply = reply.decode().strip()  ## bytes to string again and remove any leading/trailing whitespace
 
             if reply.startswith("SUCCESS:"):
                 print(f"OK → {reply}")
@@ -26,5 +26,55 @@ def send_command(host, port, command):
     except Exception as e:
         print(e)
 
+def main():
+    host = "127.0.0.1"
+    port = 5000
+
+    lectures = ["L1", "L2"]
+    tutorials = ["T1-1", "T1-2", "T2-1", "T2-2"]
+
+    while True:
+        print("\n===== MENU =====")
+        print("1. Get Lecture")
+        print("2. Get Tutorial")
+        print("5. Reset")
+        print("6. Exit")
+
+        choice = input("Enter choice: ")
+
+        if choice == "1":
+            print("Available Lectures:", lectures)
+            lec = input("Choose Lecture: ")
+
+            if lec not in lectures:
+                print("Invalid Lecture ❌")
+                continue
+
+            command = f"GET:{lec}"
+
+        elif choice == "2":
+            print("Available Tutorials:", tutorials)
+            tut = input("Choose Tutorial: ")
+
+            if tut not in tutorials:
+                print("Invalid Tutorial ❌")
+                continue
+
+            command = f"GET:{tut}"
+
+        elif choice == "5":
+            command = "RESET"
+
+        elif choice == "6":
+            print("Goodbye 👋")
+            break
+
+        else:
+            print("Invalid choice ❌")
+            continue
+
+        send_command(host, port, command)
+
+
 if __name__ == "__main__":
-    send_command("127.0.0.1", 5000, "GET:L1")
+    main()
